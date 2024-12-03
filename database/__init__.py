66debc297382f9aaeb0c2c6c36d2377cb0e2327f
tocheniy <suhanov9208@gmail.com>
2024-12-03 16:25:36 +0400

    add database module with main class and engine

---
+++ database/__init__.py
@@ -0,0 +1,3 @@
+"""Импорт класса Base из моделей для дальнейшего создания миграций через Alembic"""
+
+from database.models.dvr import Base  # noqa: F401
---
+++ database/main.py
@@ -0,0 +1,33 @@
+from sqlalchemy import inspect
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+    AsyncEngine,
+)
+from config import setting
+
+
+class Base(DeclarativeBase):
+    """Base"""
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    def to_dict(self):
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
+
+    def __repr__(self) -> str:
+        # * Получаем имена и значения всех столбцов для текущего экземпляра
+        fields = ", ".join(
+            f"{column.name}={getattr(self, column.name)!r}"
+            for column in inspect(self.__class__).c
+        )
+        # *  Возвращаем строку представления с именем класса и полями
+        return f"{self.__class__.__name__}({fields})"
+
+
+engine: AsyncEngine = create_async_engine(url=setting.db.url(), echo=setting.db.echo)
+async_session: AsyncSession = async_sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
