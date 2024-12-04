65ff1c7286ecd7a1a80d584f1a56a147a91a5e71
tocheniy <suhanov9208@gmail.com>
2024-12-04 17:27:00 +0400

    add schemas for event model

--- general/schemas.py
+++ general/schemas.py
@@ -1,11 +1,21 @@
-from pydantic import BaseModel, Field
+from datetime import datetime
+from pydantic import BaseModel, Field, ConfigDict
 
 
 class EventTo(BaseModel):
     type_of: str = Field(serialization_alias="event_type")
-    time: str = Field(serialization_alias="event_time")
+    time: datetime = Field(serialization_alias="event_time")
     dvr: str = Field(serialization_alias="dvr_name")
     camera: str | None = Field(default=None, serialization_alias="camera_name")
     city: str | None = Field(default=None, serialization_alias="city")
     chat_id: int | None = Field(default=None)
     thread: int | None = Field(default=None)
+
+
+class EventModel(BaseModel):
+    id: int | None = None
+    name: str
+    dvr_id: int
+    camera: str | None = None
+    time: datetime
+    model_config = ConfigDict(from_attributes=True)
