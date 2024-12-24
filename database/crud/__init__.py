78fd08debf886ea8066c16544bd3ab0c50568760
tocheniy <suhanov9208@gmail.com>
2024-12-24 12:57:02 +0400

    make crud for event and add event to init

---
+++ database/crud/__init__.py
@@ -0,0 +1,2 @@
+from .event import get_events_by_dvrname  # noqa: F401
+from .event import add_event  # noqa: F401
--- database/crud/event.py
+++ database/crud/event.py
@@ -1,31 +1,127 @@
+# import asyncio
+# from datetime import datetime
 import asyncio
-from datetime import datetime
+from datetime import datetime, timedelta
 import sys
-from sqlalchemy import select
+from sqlalchemy import func, select
+from sqlalchemy.sql import and_
 from sqlalchemy.exc import SQLAlchemyError
+from dateutil import parser
 
 if __name__ == "__main__":
     sys.path.append(".")
-from general.schemas import EventModel
+from general.schemas import EventDbSch, EventWithDvr
 from database.main import async_session
 from database.models.event import Event
+from database.models.dvr import Dvr
 
 
 async_session = async_session()
 
 
-async def get_all_events() -> list[Event]:
+async def get_events_by_dvrname(name: str) -> list[EventWithDvr]:
     try:
         async with async_session as session:
-            stmt = select()
+            stmt = select(
+                Event,
+                Dvr.name.label("dvr_name"),
+                Dvr.ip.label("ip"),
+                Dvr.city.label("city"),
+            ).join(Dvr, Dvr.name == name)
             res = await session.execute(stmt)
             res = res.all()
+            res = [
+                EventWithDvr(
+                    name=item.dvr_name,
+                    ip=item.ip,
+                    city=item.city,
+                    event=item.Event.to_dict(),
+                )
+                for item in res
+            ]
             return res
     except Exception as ex:
         print(ex)
 
 
-async def add_event(event: EventModel) -> Event | None:
+async def get_events_by_datetime(time: str) -> list[EventWithDvr]:
+    try:
+        first_time = parser.parse(time)
+        last_time = first_time + timedelta(hours=23, minutes=59, seconds=59)
+        async with async_session as session:
+            stmt = select(
+                Event,
+                Dvr.name.label("dvr_name"),
+                Dvr.ip.label("ip"),
+                Dvr.city.label("city"),
+            )
+            stmt = stmt.join(Dvr, Dvr.id == Event.dvr_id)
+            stmt = stmt.where((Event.time >= first_time) & (Event.time <= last_time))
+            stmt = stmt.order_by(Event.time)
+            res = await session.execute(stmt)
+            res = res.all()
+            res = [
+                EventWithDvr(
+                    name=item.dvr_name,
+                    ip=item.ip,
+                    city=item.city,
+                    event=item.Event.to_dict(),
+                )
+                for item in res
+            ]
+            return res
+    except Exception as ex:
+        print(ex)
+
+
+async def get_events_by_city_and_datetime(city: str, time: str) -> list[EventWithDvr]:
+    try:
+        first_time = parser.parse(time)
+        last_time = first_time + timedelta(hours=23, minutes=59, seconds=59)
+        async with async_session as session:
+            stmt = select(
+                Event,
+                Dvr.name.label("dvr_name"),
+                Dvr.ip.label("ip"),
+                Dvr.city.label("city"),
+            )
+            stmt = stmt.join(Dvr, Dvr.id == Event.dvr_id)
+            filters = and_(
+                Dvr.city == city,
+                Event.time >= first_time,
+                Event.time <= last_time,
+            )
+            stmt = stmt.where(filters)
+            stmt = stmt.order_by(Event.time)
+            res = await session.execute(stmt)
+            res = res.all()
+            res = [
+                EventWithDvr(
+                    name=item.dvr_name,
+                    ip=item.ip,
+                    city=item.city,
+                    event=item.Event.to_dict(),
+                )
+                for item in res
+            ]
+            return res
+    except Exception as ex:
+        print(ex)
+
+
+async def get_all_events() -> list[EventDbSch]:
+    try:
+        async with async_session as session:
+            stmt = select(Event)
+            res = await session.execute(stmt)
+            res = res.scalars().all()
+            res = [EventDbSch.model_validate(item) for item in res]
+            return res
+    except Exception as ex:
+        print(ex)
+
+
+async def add_event(event: EventDbSch) -> Event | None:
     async with async_session as session:
         event = event.model_dump(exclude_none=True)
         data = Event(**event)
@@ -36,28 +132,40 @@ async def add_event(event: EventModel) -> Event | None:
             await session.rollback()
             raise ex
         return data
-        # res = await session.execute(stmt)
-        # res = res.all()
-        # return res
 
 
 async def main():
+    # events = await get_events_by_dvrname("Reg 5 VLG")
+    # print(events)
+
     # events = await get_all_events()
     # print(events)
-    ev = EventModel(
-        # id=1,
-        name="dasdasda",
-        dvr_id=1,
-        time=datetime.now(),
-    )
-    # print(ev)
-    event = await add_event(ev)
-    print(event)
-    # dvr = await get_dvr_by_name("Reg 6 VLG")
-    # print(dvr)
-    # await async_session.close()
+
+    events = await get_events_by_datetime("14.12.2024")
+    print(len(events))
+    print(events[0])
+    print(events[-1])
+    
+    events = await get_events_by_city_and_datetime("MSK", "15.12.2024")
+    print(len(events))
+    print(events[0])
+    print(events[-1])
+
+
+#     ev = EventModel(
+#         # id=1,
+#         name="dasdasda",
+#         dvr_id=1,
+#         time=datetime.now(),
+#     )
+#     # print(ev)
+#     event = await add_event(ev)
+#     print(event)
+#     # dvr = await get_dvr_by_name("Reg 6 VLG")
+#     # print(dvr)
+#     # await async_session.close()
 
 
 if __name__ == "__main__":
-    # asyncio.get_event_loop().run_until_complete(main())
-    asyncio.run(main())
+    asyncio.get_event_loop().run_until_complete(main())
+#     asyncio.run(main())
