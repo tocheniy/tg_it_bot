de3b3bd65bc73250c46ac78bc37209e886f11b6a
tocheniy <suhanov9208@gmail.com>
2024-12-24 17:16:05 +0400

    relation cities with dvrs

--- database/models/city.py
+++ database/models/city.py
@@ -10,3 +10,4 @@ class City(Base):
     region: Mapped[str] = mapped_column(String(30), nullable=True)
     chat_id: Mapped[int] = mapped_column(ForeignKey("chats.id"), nullable=False)
     chat: Mapped["Chat"] = relationship(back_populates="cities")  # type: ignore # noqa: F821
+    dvrs: Mapped[list["Dvr"]] = relationship(back_populates="city_tmp")  # type: ignore # noqa: F821
--- database/models/dvr.py
+++ database/models/dvr.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database.main import Base
 
@@ -10,4 +10,7 @@ class Dvr(Base):
     login: Mapped[str] = mapped_column(String(16), nullable=False)
     password: Mapped[str] = mapped_column(String(20), nullable=False)
     city: Mapped[str] = mapped_column(String(20), nullable=False)
+    city_id: Mapped[int] = mapped_column(ForeignKey("cities.id"), nullable=True)
+    
     events: Mapped[list["Event"]] = relationship(back_populates="dvr")  # type: ignore  # noqa: F821
+    city_tmp: Mapped["City"] = relationship(back_populates="dvrs")  # type: ignore  # noqa: F821
