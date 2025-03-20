1dda91086b5dcd69b160fe4cfdcf5e4640d9cacf
tocheniy <suhanov9208@gmail.com>
2025-03-20 23:37:57 +0400

    change take_event and mo

--- general/scripts.py
+++ general/scripts.py
@@ -1,4 +1,5 @@
 # scripts
+import asyncio
 import sys
 
 # import asyncio
@@ -6,13 +7,11 @@ from datetime import datetime
 import logging
 import re
 
-from database.models.event import Event
-
 if __name__ == "__main__":
     sys.path.append(".")
 from database.crud.dvr import get_dvr_by_name
 from database.crud.event import add_event
-from general.schemas import EventDbSch, EventTgSch
+from general.schemas import EventDbSch, EventTgSch, EventWithChat
 from config import setting
 
 logging.basicConfig(level=logging.DEBUG)
@@ -30,8 +29,6 @@ def take_event(msg) -> EventTgSch | list[EventTgSch] | None:
     dvr_name = ""
     camera_name = ""
     city = ""
-    chat_id = 0
-    thread = 0
     cameras_names = []
     result = []
     ev: EventTgSch | None = None
@@ -55,25 +52,11 @@ def take_event(msg) -> EventTgSch | list[EventTgSch] | None:
             pattern = r"((\w+|\w+\s\w+)(\([A-Z]\d+\)))"
             cameras_names = [item[0] for item in re.findall(pattern, camera_name)]
 
-    chats = setting.chats.result
-    city = city.lower()
-    if city not in ["msk", "vlg"]:
-        city = "other"
-    city_chat = chats.get(city)
-    if city_chat is None:
-        return None
-    chat_id = city_chat.get("chat_id")
-
-    ev_type_for_thread = event_type.lower().replace(" ", "_")
-    thread = city_chat.get("thread").get(ev_type_for_thread)
-
     ev = EventTgSch(
         type_of=event_type,
         time=event_time,
         dvr=dvr_name,
         city=city,
-        chat_id=chat_id,
-        thread=thread,
     )
 
     if cameras_names and len(cameras_names) >= 2:
@@ -91,50 +74,68 @@ def take_event(msg) -> EventTgSch | list[EventTgSch] | None:
         return ev
 
 
-def get_one_event(ev: EventTgSch | list[EventTgSch]) -> EventTgSch:
-    if isinstance(ev, list):
-        return ev[0]
-    return ev
-
-
 async def define_event_and_add_to_database(
     events: EventTgSch | list[EventTgSch],
-) -> list[Event] | Event | None:
+) -> list[EventWithChat] | EventWithChat | None:
     if isinstance(events, list) and len(events) >= 2:
         res = []
         for evnt in events:
-            event_from_db = await add_event_to_db(evnt)
-            if not event_from_db:
+            event_with_chat = await add_event_to_db_and_return_event_with_chat(evnt)
+            if not event_with_chat:
                 continue
-            res.append(event_from_db)
+            res.append(event_with_chat)
             display_event_log(evnt)
         if not res:
             return None
         return res
 
     elif isinstance(events, EventTgSch):
-        event_from_db = await add_event_to_db(events)
-        if not event_from_db:
+        event_with_chat = await add_event_to_db_and_return_event_with_chat(events)
+        if not event_with_chat:
             return None
         display_event_log(events)
-        return event_from_db
+        return event_with_chat
 
 
-async def add_event_to_db(event: EventTgSch) -> Event | None:
+async def add_event_to_db_and_return_event_with_chat(
+    event: EventTgSch,
+) -> EventWithChat | None:
     dvr = await get_dvr_by_name(event.dvr)
     if not dvr:
         return
-    event = EventDbSch(
+    division = dvr.division
+    chat = division.chat
+    event_to_db = EventDbSch(
         dvr_id=dvr.id,
         name=event.type_of,
         camera=event.camera,
         time=event.time,
     )
+    event_with_chat = EventWithChat(chat=chat, event=event_to_db)
+    # * for test
+    test = "TEST"
+    width = 18
+    symbol = "_-*-_"
+    symbol_quant = 5
+    test_header = (
+        f"{symbol * symbol_quant:<20}{test:^{width}}{symbol * symbol_quant:>20}"
+    )
+    if event.type_of == test:
+        print(test_header)
+        print(event_with_chat.event)
+        print(event_with_chat.chat)
+        symbol = "¯-*-¯"
+        test_header = (
+            f"{symbol * symbol_quant:<20}{test:^{width}}{symbol * symbol_quant:>20}"
+        )
+        print(test_header)
+        print()
+        return event_with_chat
 
-    event = await add_event(event)
-    if not event:
+    event_from_db = await add_event(event_to_db)
+    if not event_from_db:
         return None
-    return event
+    return event_with_chat
 
 
 def display_event_log(event: EventTgSch):
@@ -150,25 +151,43 @@ def display_event_log(event: EventTgSch):
     logging.info(log_text)
 
 
-# async def main():
-#     ev = [
-#         EventTo(
-#             type_of="dsadas",
-#             time=datetime.now(),
-#             camera="Cam",
-#             dvr="Reg 1 VLG",
-#             city="MSK",
-#         ),
-#         EventTo(
-#             type_of="dsadas",
-#             time=datetime.now(),
-#             camera="Cam",
-#             dvr="Reg 1 VLG",
-#             city="MSK",
-#         ),
-#     ]
-#     await define_event_and_add_to_database(ev)
+async def get_chat_with_thread_from_event(event_with_chat: EventWithChat):
+    event = event_with_chat.event
+    chat = event_with_chat.chat
+
+    type_name = event.name.lower().replace(" ", "_")
+    chat_id = chat.tg_chat_id
+    thread = chat.__getattribute__(type_name)
+    return chat_id, thread
 
 
-# if __name__ == "__main__":
-#     asyncio.run(main())
+async def main():
+    ev = [
+        EventTgSch(
+            type_of="TEST",
+            time=datetime.now(),
+            camera="Cam",
+            dvr="Reg 1 VLG",
+            city="MSK",
+        ),
+        EventTgSch(
+            type_of="TEST",
+            time=datetime.now(),
+            camera="Cam",
+            dvr="Reg 1 MSK",
+            city="MSK",
+        ),
+        EventTgSch(
+            type_of="TEST",
+            time=datetime.now(),
+            camera="Cam",
+            dvr="Reg 1 STAR POLT",
+            city="STAR POLT",
+        ),
+    ]
+    await define_event_and_add_to_database(ev)
+
+
+if __name__ == "__main__":
+    asyncio.new_event_loop().run_until_complete(main())
+    # asyncio.run(main())
