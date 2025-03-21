8fa8071872f3272b576ea1aaa234ad75a99a0477
tocheniy <suhanov9208@gmail.com>
2025-03-21 13:51:00 +0400

    add statistic field and migration

--- database/models/chat.py
+++ database/models/chat.py
@@ -6,9 +6,10 @@ from database.models.division import Division
 
 class Chat(Base):
     __tablename__ = "chats"
-    name: Mapped[str] = mapped_column(String(30), nullable=True)
-    tg_chat_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
-    hdd_error: Mapped[int] = mapped_column(Integer, nullable=False)
-    view_tampering: Mapped[int] = mapped_column(Integer, nullable=False)
-    video_signal_lost: Mapped[int] = mapped_column(Integer, nullable=False)
+    name: Mapped[str | None] = mapped_column(String(30))
+    tg_chat_id: Mapped[int] = mapped_column(BigInteger)
+    hdd_error: Mapped[int] = mapped_column(Integer)
+    view_tampering: Mapped[int] = mapped_column(Integer)
+    video_signal_lost: Mapped[int] = mapped_column(Integer)
+    statistic: Mapped[int | None] = mapped_column(Integer)
     divisions: Mapped[list["Division"]] = relationship(back_populates="chat")
