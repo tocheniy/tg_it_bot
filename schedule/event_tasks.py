9fabe10db23f905fa3d596cdafe88719336c5b4e
tocheniy <suhanov9208@gmail.com>
2025-03-21 17:11:13 +0400

    rework send statistic

--- schedule/event_tasks.py
+++ schedule/event_tasks.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from telethon import TelegramClient
 from database.crud.chat import get_chats
 from database.crud.event import get_events_by_datetime_and_chat
+from general.schemas import StatSch
 from utils.data_work import make_statistic
 
 
@@ -38,11 +39,26 @@ async def send_statistics(ctx):
 
             res.append(ev_for_graphic)
 
-        files_src = make_statistic(res, tg_chat_id, prev_day, chat.name)
-        cap = f"Статистика {chat.name} за {prev_day}"
+        stats = make_statistic(res, tg_chat_id, prev_day, chat.name)
+
+        # return
+        cap = f"Статистика {chat.name} за {prev_day}\n\n"
+        cap += f"{'-' * 20}\n"
+        # cap = ""
+
+        def get_cap_text(cap: str, stats: list[StatSch]):
+            for stat in stats:
+                cap += f"{stat.event_type} | кол-во: {stat.count}\n"
+                cap += f"{stat.data}\n"
+                cap += f"{'-' * 20}\n"
+
+            return cap
+
+        cap = get_cap_text(cap, stats)
+
         await client.send_file(
             tg_chat_id,
             caption=cap,
-            file=files_src,
+            file=[stat.file_name for stat in stats],
             reply_to=stat_thread,
         )
